bcc035dfc61ba4ec92947af60fb052c266c8f161
chinchuna <chinchunachinchu@gmail.com>
2017-12-06 16:14:57 +0530

    Create messageComponent and add a service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageService } from './message.service';
 
 
 @NgModule({
@@ -17,7 +18,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
