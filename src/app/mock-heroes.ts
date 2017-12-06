3e63180c004386a7c755dd5abb470ebcf3be0826
chinchuna <chinchunachinchu@gmail.com>
2017-12-06 14:01:15 +0530

    Creating mock heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [{id: 11, name: 'Manu'} ,
+                            {id: 12, name: 'Mithun'} ,
+                            { id: 13, name: 'Vishnu'},
+                            {id: 14, name: 'Ramu'},
+                            { id: 15, name: 'Ajay'},
+                            { id: 16, name: 'Siva'}];
